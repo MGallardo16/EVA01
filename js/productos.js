e6a1a3403f743cb146519e6c21ce1e0ec9c115f7
MGallardo16 <marc.gallardos@duocuc.cl>
2026-09-02 21:30:45 -0400

    Integración completa de productos, total de 25. Se actualizó también de buena forma el script en productos_e.html para que se pueda ver la info.

--- js/productos.js
+++ js/productos.js
@@ -52,7 +52,135 @@ const productos =[
         precio: 7990,
         imagen: "https://media.falabella.com/falabellaCL/140395813_04/w=1500,h=1500,fit=cover",
         descripcion: "Juego de Mesa Ludo / Parchís Clásico – ¡Diversión Garantizada para Toda la Familia! <br> Revive los mejores momentos en familia con el infaltable juego de recorrido y estrategia Ludo / Parchís Clásico. Con su colorido tablero de cuatro zonas (amarillo, azul, rojo y verde) y sus piezas de gran visibilidad, es el juego ideal para introducir a los más pequeños en los juegos de mesa o para disfrutar de una tarde llena de competencia amigable."
+      },
+      {
+          id: 8,
+          nombre: "Ruleta de tragos",
+          precio: 14990,
+          imagen: "https://i5.walmartimages.cl/asr/19f5841c-b71a-4551-a750-3014f6eeefa6.77c1a270820248afd44ab8ecefb759e3.jpeg?odnHeight=2000&odnWidth=2000&odnBg=ffffff",
+          descripcion: "Ruleta de Tragos / Shots (Realplay) – ¡El Juego Definitivo para tus Fiestas! <br> Transforma cualquier previa o reunión en una noche inolvidable con la Ruleta de Chupitos Realplay. Inspirada en el clásico juego de casino, esta ruleta combina azar, emoción y tus bebidas favoritas para romper el hielo y garantizar la máxima diversión entre amigos."
+      },
+      {
+          id: 9,
+          nombre: "Harmonies",
+          precio: 29990,
+          imagen: "https://gatoarcano.cl/wp-content/uploads/2024/02/4-2.jpg",
+          descripcion: "Harmonies (Libellud / Johan Benvenuto) – Crea Mundos Vibrantemente Armónicos <br> Crea paisajes deslumbrantes y da la bienvenida a increíbles criaturas con Harmonies, el aclamado juego de mesa de estrategia táctica y construcción de patrones. Combina fichas de colores para modelar montañas, ríos, bosques y llanuras, creando los hábitats perfectos para que los animales habiten tu ecosistema."
+      },
+      {
+          id: 10,
+          nombre: "The Castles of Burgundy",
+          precio: 59990,
+          imagen: "https://www.updown.cl/wp-content/uploads/2024/12/standard_resolution-460.jpg",
+          descripcion:"The Castles of Burgundy (Alea / Stefan Feld) – El Obra Maestra del Eurogame Clásico <br> Viaja a la Francia del siglo XV y asume el rol de un aristócrata influyente con The Castles of Burgundy (Los Castillos de Borgoña). Diseñado por el legendario Stefan Feld, este aclamado juego de estrategia te desafía a expandir tu principado mediante el comercio, la ganadería, la edificación de ciudades y el desarrollo de tecnologías clave."
+      },
+      {
+          id: 11,
+          nombre: "DUNE: IMPERIUM - UPRISING",
+          precio: 110990,
+          imagen: "https://i5.walmartimages.cl/asr/fa6cb306-2441-4e58-92ef-aefd050bd8b6.86bc07fdceb33eaa0ffa5cb1d1c6e0a9.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF",
+          descripcion: "Dune: Imperium – Uprising – El Control del Desierto y el Imperio en tus Manos <br> Regresa al peligroso planeta desértico de Arrakis con Dune: Imperium – Uprising. Esta secuela independiente (standalone) lleva la aclamada fórmula de colocación de trabajadores y construcción de mazos a un nuevo nivel, introduciendo batallas más intensas, gusanos de arena montables y nuevas facciones para tomar el control del Imperio."
+      },
+      {
+          id: 12,
+          nombre: "TERRAFORMING MARS",
+          precio: 63990,
+          imagen: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZobICe3dr0c_Lcr2EEtfnAoz01-vAG0XQRbm6kbJpoUB0_LUwPLrufRlP&s=10",
+          descripcion: "Terraforming Mars (FryxGames / Jacob Fryxelius) – Convierte el Planeta Rojo en un Nuevo Hogar <br> Lidera una corporación con grandes recursos y encabeza la mayor obra de la humanidad en Terraforming Mars. En este aclamado juego de estrategia científica y gestión de motor de proyectos, tu objetivo será aumentar la temperatura, elevar los niveles de oxígeno y crear océanos para hacer que Marte sea un planeta habitable para las futuras generaciones."
+      },
+      {
+          id: 13,
+          nombre: "THE OLD KINGS CROWN",
+          precio: 62990,
+          imagen: "https://imgcdn.gamefound.com/productimage/projects/2895/b68418bb-ba3c-4808-975f-bbdf09f43191.png",
+          descripcion: "The Old King’s Crown (Earthy Games) – Una Historia de Realeza, Rebeldes y Astucia <br> Adéntrate en un reino en ruinas y reclama el trono vacío en The Old King’s Crown. En este fascinante e ilustrado juego de cartas, estrategia y control de territorio, te enfrentarás a tus rivales utilizando faroles, astucia táctica y la fuerza de tu facción para coronarte como el nuevo soberano."
+        },
+      {
+          id: 14,
+          nombre: "SCRABBLE 2 en 1",
+          precio: 19990,
+          imagen: "https://i5.walmartimages.cl/asr/366126fa-93b0-45c8-a615-047aa385b47a.8ad5828f5c12ad25a4c3236f554179c7.jpeg",
+          descripcion: "Scrabble 2 Juegos en 1: Clásico y Colaborativo (Mattel Games) – ¡Doble Formas de Desafiar tu Vocabulario! <br> Pon a prueba tu agilidad mental y riqueza léxica con el emblemático Scrabble, que ahora regresa renovado con una emocionante modalidad de juego extra. Con un tablero de doble cara, podrás elegir entre la competencia competitiva tradicional o trabajar en equipo en la nueva experiencia colaborativa."
+      },
+      {
+          id: 15,
+          nombre: "¡QUE DICE CHILE!",
+          precio: 11990,
+          imagen: "https://ansaldo.cl/cdn/shop/files/36020_02.jpg?v=1745860328&width=1200",
+          descripcion: "¡Qué Dice Chile! Juego de Mesa – ¡Siente la Experiencia del Exitoso Programa de TV en tu Casa! <br> Trae toda la emoción, las risas y la adrenalina del exitoso programa de televisión directamente a tu living con el juego de mesa oficial de ¡Qué Dice Chile!. Reúne a tu familia o grupo de amigos, divídanse en equipos y descubran cuáles fueron las respuestas más populares de los chilenos a las preguntas más insólitas y divertidas."
+      },
+      {
+          id: 16,
+          nombre: "Monos Locos",
+          precio: 14990,
+          imagen: "https://fotosol.cl/cdn/shop/files/MonosLocosMattel_4.jpg?v=1691083586",
+          descripcion:"Monos Locos (Mattel Games) – ¡Cuidado con la Caída de la Manada - <br> Añade tensión y muchas risas a tus tardes con Monos Locos, el clásico juego de habilidad y pulso firme donde un solo movimiento en falso puede hacer que los simios se vengan abajo. Es el juego ideal para la infancia y para compartir partidas rápidas en familia."
+      },
+      {
+          id: 17,
+          nombre: "Connect 4 Clásico",
+          precio: 12990,
+          imagen: "https://cdn.playtherapysupply.com/img/f/2fab9ff18a90.jpg",
+          descripcion:"Connect 4 / Conecta 4 Clásico (Hasbro Gaming) – ¡El Desafío Estratégico Frente a Frente! <br> Desafía a tus amigos o familiares a una competencia rápida y llena de estrategia con el icónico Connect 4 (Conecta 4). El objetivo es simple, pero dominarlo requiere astucia: sé el primero en alinear cuatro fichas de tu color en línea horizontal, vertical o diagonal mientras bloqueas los intentos de tu oponente."
+      },
+      {
+          id: 18,
+          nombre: "¿Quién es Quien?",
+          precio: 13990,
+          imagen: "https://promociones.diariovasco.com/media/catalog/product/cache/801d993861ca136d487637ef4b70ec65/4/2/4292_y74aewdmqrgfolqm.jpg",
+          descripcion:"¿Quién es Quién? (Hasbro Gaming) – ¡El Original Juego de Adivinar Personajes! <br> Descubre la identidad del personaje misterioso de tu oponente antes de que adivine el tuyo con ¿Quién es Quién?, el inolvidable juego de deducción y preguntas inteligentes. Haz preguntas astutas que solo se respondan con 'sí' o 'no', descarta rostros bajando las pestañas del tablero y resuelve el misterio."
+      },
+      {
+          id: 19,
+          nombre: "Taca taca sobremesa",
+          precio: 24990,
+          imagen: "https://dojiw2m9tvv09.cloudfront.net/17766/product/tacataca-60639.jpg",
+          descripcion:"Mini Taca Taca / Taca Taca de Sobremesa – ¡La Pasión del Fútbol en Tamaño Compacto! <br> Lleva la emoción de la cancha a cualquier rincón de tu hogar, oficina o sala de juegos con este increíble Mini Taca Taca de Madera. Diseñado con una sólida estructura estilo madera, ofrece partidos dinámicos, llenos de goles, reflejos y adrenalina en un formato liviano y fácil de transportar."
+      },
+      {
+          id: 20,
+          nombre: "Mala leche",
+          precio: 15990,
+          imagen: "https://i5.walmartimages.cl/asr/ac41861c-0811-4f64-b1e0-a4d0545a38de.7264de84e38050b6d390e5e37566429d.png?odnHeight=612&odnWidth=612&odnBg=FFFFFF",
+          descripcion:"Mala Leche: El Original – Juego de Humor Negro Chileno <br> Saca tu lado más políticamente incorrecto con Mala Leche, el icónico juego de cartas chileno diseñado para desatar risas sin filtro. Con un formato inspirando en la clásica caja de leche de cartón, esta propuesta te invita a completar frases absurdas, subir el tono de la fiesta y romper cualquier tipo de tabú entre amigos."
+      },
+      {
+          id: 21,
+          nombre: "Cubo rubik 3x3x3",
+          precio: 5990,
+          imagen: "https://rimage.ripley.cl/home.ripley/Attachment/WOP/1/2000384533413/Image-2000384533413.",
+          descripcion: "Cubo Rubik 3x3 Clásico (Rubik's / Spin Master) – ¡El Rompecabezas Más Famoso del Mundo! <br> Pon a prueba tu agilidad mental, lógica y paciencia con el genuino Cubo de Rubik 3x3. Este rompecabezas tridimensional ha desafiado a generaciones enteras y sigue siendo el rey indiscutible de los acertijos mecánicos. Gira sus caras, combina sus colores y encuentra el camino para devolver cada lado a su color original."
+      },
+      {
+          id: 22,
+          nombre: "Monopoly Star Wars",
+          precio: 25990,
+          imagen: "https://i.ebayimg.com/images/g/KPgAAeSwoxhqJ5rW/s-l1200.webp",
+          descripcion: "Monopoly: Star Wars The Complete Saga Edition (Hasbro Gaming) – ¡Domina la Galaxia de las 9 Películas! <br> Embárcate en un viaje galáctico definitivo con Monopoly: Star Wars The Complete Saga Edition. Esta edición de colección rinde homenaje a toda la historia de la saga Skywalker, reuniendo planetas, droides, naves y momentos icónicos desde el Episodio I: La Amenaza Fantasma hasta el Episodio IX: El Ascenso de Skywalker en una experiencia de juego épica para fanáticos de todas las edades."
+
+      },
+      {
+          id: 23,
+          nombre: "Battleship",
+          precio: 222990,
+          imagen: "https://hasbrostore.cl/15428-large_default/juego-de-mesa-hasbro-gaming-battleship.jpg",
+          descripcion: "Battleship / Batalla Naval (Hasbro Gaming) – ¡Estrategia, Deducción y Combate en Alta Mar! <br> Lidera tu flota hacia la victoria y hunde los barcos de tu rival antes de que detecten tus coordenadas con Battleship, el emblemático juego de estrategia y combate naval. Despliega tu flota en secreto, rastrea los mares enemigos celda a celda y lanza ataques precisos en un duelo cara a cara donde la táctica y la intuición marcan la diferencia."
+      },
+      {
+          id: 24,
+          nombre: "Simon Clásico",
+          precio: 26990,
+          imagen: "https://hasbrostore.cl/12005-thickbox_default/simon-clasico.jpg",
+          descripcion: "Simon Clásico (Hasbro Gaming) – ¡Pon a Prueba tu Memoria con el Legendario Juego de Luces y Sonidos! <br> Desafía tus límites de concentración y reflejos con el inconfundible Simon, el juego electrónico de memoria por excelencia. Observa las secuencias de luces de colores, memoriza los patrones de sonidos y repítelos en el orden correcto a medida que el ritmo se acelera y la dificultad aumenta."
+      },
+      {
+          id: 25,
+          nombre: "Dominó",
+          precio: 6990,
+          imagen: "https://toyng.cl/cdn/shop/files/juego-de-mesa-domino-clasico-toyng-7651606.png?v=1777364591",
+          descripcion: "Dominó Clásico Doble 6 en Caja de Madera (Set of 28 Dominoes) – ¡Tradición y Estrategia para Toda la Familia! <br> Disfruta de uno de los juegos de mesa más populares y tradicionales de la historia con este Set Clásico de Dominó Doble 6. Con fichas resistentes y una elegante caja contenedora de madera con tapa deslizable, este conjunto es perfecto para reuniones familiares, tardes de juego entre amigos o para llevar de viaje a cualquier lugar."
       }
+
     ];
 
 
